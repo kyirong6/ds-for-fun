840ff0150329bbfb286ae61527883e7e75adbff1
Choenden Kyirong <choendenkyirong@d207-023-199-027.wireless.sfu.ca>
2019-03-04 15:58:23 -0800

    added files in dynamic array section

---
+++ choenden/arrays/insertionSort.py
@@ -0,0 +1,9 @@
+def insertionSort(A):
+    """Sort list in nondecresing order"""
+    for k in range(1, len(A)):
+        cur = A[k]
+        j = k
+        while j>0 and A[j-1] > cur:
+            A[j] = A[j-1]
+            j -= 1
+        A[j] = cur
